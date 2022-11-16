2396045fb99a7d37e1a4ba0a278184d2eea283d6
Amrit <35948782+Amrit54300@users.noreply.github.com>
2022-11-16 14:00:01 +0530

    Update script.js

--- module4/script.js
+++ module4/script.js
@@ -1,6 +1,6 @@
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 for (var name in names) {
-  var firstLetter = name.charAt(0);
+  var firstLetter = names[name].charAt(0);
   if ( firstLetter.toLowerCase() == 'j') {
     byeSpeaker.speak(names[name]);
   } else {
