fcb3bf470fe4fd2976eebbfb2e460f269bcdef76
Amrit <35948782+Amrit54300@users.noreply.github.com>
2022-11-16 14:02:30 +0530

    Update script.js

--- module4/script.js
+++ module4/script.js
@@ -1,9 +1,9 @@
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-for (var name in names) {
-  var firstLetter = names[name].charAt(0);
-  if ( firstLetter.toLowerCase() == 'j') {
-    byeSpeaker.speak(names[name]);
+for (var i = 0; i < names.length(); i++) {
+  var firstLetter = names[i].charAt(0).toLowerCase()
+  if ( firstLetter == 'j') {
+    byeSpeaker.speak(names[i]);
   } else {
-    helloSpeaker.speak(names[name]);
+    helloSpeaker.speak(names[i]);
   }
 }
